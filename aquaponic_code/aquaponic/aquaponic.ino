a5ef88ed50c4715195458016d204e198befffc35
ArthurSZANTYR <szantyrarthur.pto@gmail.com>
2023-12-06 18:31:23 +0100

    modif timer pump

--- aquaponic_code/aquaponic/aquaponic.ino
+++ aquaponic_code/aquaponic/aquaponic.ino
@@ -3,14 +3,23 @@ const int led1Pin = 9;
 
 //pompe time control
 unsigned long previousMillisPump = 0; // Variable pour suivre le temps écoulé pour la pompe
-const long pumpOnDuration = 580000; //90000; // Durée d'activation de la pompe (1 minute 30 secondes en millisecondes)
-const long pumpOffDuration = 60000; // Durée de désactivation de la pompe (15 minutes en millisecondes)
+const long pumpOnDuration = 180000; //3min30 //90000; // Durée d'activation de la pompe (1 minute 30 secondes en millisecondes)
+const long pumpOffDuration = 240000; //4min30 // Durée de désactivation de la pompe (15 minutes en millisecondes)
 bool pompeActive = false;
 
 //led1 time control
 unsigned long previousMillisLED = 0;
 const long ledOffDuration = 90000;//28800000; // Durée de 8 heures pour la LED éteinte (en millisecondes)
 const long ledOnDuration = 90000;//57600000; // Durée de 16 heures pour la LED allumée (en millisecondes)
+
+// Setup for the stepper motor
+const int stepPin = 2;
+const int dirPin = 15;
+const int sleepPin = 16;
+const int enablePin = 4;
+const int stepsPerRev = 200;
+int pulseWidthMicros = 1000;  // microseconds
+int millisBtwnSteps = 10000;
 
 
 void setup() {
@@ -47,4 +56,30 @@ void loop() {
   }
 
 
+}
+
+// function to turn the stepper motor and nurrish fish
+void nourish() {
+  //turn on the stepper
+  //Serial.println(F("Distribution of food"));  
+  //digitalWrite(LED_v, HIGH);
+
+  //wake up the stepper
+  digitalWrite(sleepPin, HIGH);
+  digitalWrite(enablePin, LOW);
+  delay(2000);
+  
+  digitalWrite(dirPin, LOW);  // Enables the motor to move in a particular direction
+    // Makes 200 pulses for making one full cycle rotation
+  for (int i = 0; i < 1 * stepsPerRev; i++) 
+  {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(pulseWidthMicros);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(millisBtwnSteps);
+  }
+  //turn off the stepper
+  digitalWrite(sleepPin,LOW);
+  digitalWrite(enablePin, HIGH);
+  //digitalWrite(LED_v, LOW);
 }
