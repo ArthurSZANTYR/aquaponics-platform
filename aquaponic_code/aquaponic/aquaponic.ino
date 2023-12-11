f42bada2581fccc6166f1d2613e46a5bef4fcd41
ArthurSZANTYR <szantyrarthur.pto@gmail.com>
2023-12-11 15:29:10 +0100

    update timer

--- aquaponic_code/aquaponic/aquaponic.ino
+++ aquaponic_code/aquaponic/aquaponic.ino
@@ -1,16 +1,22 @@
-const int pompePin = 2;
-const int led1Pin = 9;
-
 //pompe time control
+const int pompePin = 2;
 unsigned long previousMillisPump = 0; // Variable pour suivre le temps écoulé pour la pompe
 const long pumpOnDuration = 180000; //3min30 //90000; // Durée d'activation de la pompe (1 minute 30 secondes en millisecondes)
 const long pumpOffDuration = 240000; //4min30 // Durée de désactivation de la pompe (15 minutes en millisecondes)
 bool pompeActive = false;
 
 //led1 time control
+const int led1Pin = 9;
 unsigned long previousMillisLED = 0;
-const long ledOffDuration = 90000;//28800000; // Durée de 8 heures pour la LED éteinte (en millisecondes)
-const long ledOnDuration = 90000;//57600000; // Durée de 16 heures pour la LED allumée (en millisecondes)
+const long ledOffDuration = 43200000  ; //12H//8 min;//28800000; // Durée de 8 heures pour la LED éteinte (en millisecondes)
+const long ledOnDuration = 43200000; //16 min  //57600000; // Durée de 16 heures pour la LED allumée (en millisecondes)
+
+//led2 time control
+const int led2Pin = 3;
+//unsigned long previousMillisLED = 0;
+//const long ledOffDuration = 43200000;//8 min;//28800000; // Durée de 8 heures pour la LED éteinte (en millisecondes)
+//const long ledOnDuration = 43200000; //16 min  //57600000; // Durée de 16 heures pour la LED allumée (en millisecondes)
+
 
 // Setup for the stepper motor
 const int stepPin = 2;
@@ -50,6 +56,19 @@ void loop() {
   } else if (currentMillis - previousMillisLED <= ledOffDuration + ledOnDuration) {
     // Si moins de 16 heures au total se sont écoulées, allumer la LED
     analogWrite(led1Pin, 127); // Allumer la LED
+  } else {
+    // Réinitialiser le temps pour recommencer le cycle 24 heures
+    previousMillisLED = currentMillis;
+  }
+
+  //control led2
+  // Contrôle de la LED pour 16 heures (Allumée) après 8 heures (Éteinte)
+  if (currentMillis - previousMillisLED <= ledOffDuration) {
+    // Si moins de 8 heures se sont écoulées, garder la LED éteinte
+    analogWrite(led2Pin, 0); // Éteindre la LED
+  } else if (currentMillis - previousMillisLED <= ledOffDuration + ledOnDuration) {
+    // Si moins de 16 heures au total se sont écoulées, allumer la LED
+    analogWrite(led2Pin, 127); // Allumer la LED
   } else {
     // Réinitialiser le temps pour recommencer le cycle 24 heures
     previousMillisLED = currentMillis;
