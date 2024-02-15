e8239f4c9be0451ac74ebd63134bf54392aba220
ArthurSZANTYR <szantyrarthur.pto@gmail.com>
2024-02-15 15:44:42 +0100

    communication json to html

--- webserver/script.js
+++ webserver/script.js
@@ -1,1 +1,28 @@
+//////////////////////  ANIMATION PAGE ////////////////////////
+
 const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener('click', () =>{
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () =>{
+    sideMenu.style.display = 'none';
+})
+
+//////////////////  DATA LOAD  ////////////////////////////
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchDataAndUpdateUI();
+});
+
+function fetchDataAndUpdateUI() {
+    fetch('./data.json') // Assurez-vous que le chemin vers data.json est correct
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('.temperature .middle .left h1').innerText = `${data.temperature}°C`;
+            document.querySelector('.tds .middle .left h1').innerText = `${data.tds}ppm`;
+        })
+}
