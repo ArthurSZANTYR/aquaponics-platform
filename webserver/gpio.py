2303cac6baf7533d87344928d8d5a4eda47c12c6
ArthurSZANTYR <szantyrarthur.pto@gmail.com>
2024-02-16 19:18:19 +0100

    gpio test

--- webserver/gpio.py
+++ webserver/gpio.py
@@ -4,56 +4,51 @@ import RPi.GPIO as GPIO
 import json  # Importer le module json
 
 # Pins Setup
-led1Pin = 13
-led2Pin = 3
-led3Pin = 4
-pompePin = 26
+pump1Pin = 26
 tdsPin = 24
 
 # GPIO setup
-GPIO.setmode(GPIO.BCM)
-GPIO.setup([led1Pin, led2Pin, led3Pin, pompePin], GPIO.OUT)
 GPIO.setup([tdsPin], GPIO.IN)
-
-# Création des objets PWM pour chaque LED
-pwm_led1 = GPIO.PWM(led1Pin, 1000)  # Fréquence de 1000 Hz
-pwm_led2 = GPIO.PWM(led2Pin, 1000)
-pwm_led3 = GPIO.PWM(led3Pin, 1000)
-
-# Démarrage des PWM avec un duty cycle de 0 (LED éteinte)
-pwm_led1.start(0)
-pwm_led2.start(0)
-pwm_led3.start(0)
-
-#TDS setup
-VREF = 5.0
-ADC_RESOLUTION = 1024.0
+GPIO.setup([pump1Pin], GPIO.OUT)
 
 # Chemin du fichier JSON
 fichier_json = 'data.json'
 
-def read_pump_interval():
+def read_pump1_values():    
     with open(fichier_json, 'r') as file:
         data = json.load(file)
-        return int(data.get('pumpInterval', 0))  # Utilisez une valeur par défaut de 0 si non trouvé
+        pump1OnValue = data.get('pump1OnValue', 0)  # 0 est une valeur par défaut si la clé n'existe pas
+        pump1OffValue = data.get('pump1OffValue', 0)
+        
+    return pump1OnValue, pump1OffValue
 
+def pump1_status():
+    time_on, time_off = read_pump1_values()
 
-def set_led_intensity(led_pwm, intensity):
-    # Convertir la valeur d'intensité (0-10) en pourcentage (0-100)
-    duty_cycle = intensity * 10
-    led_pwm.ChangeDutyCycle(duty_cycle)
+    now = datetime.datetime.now()
+    
+    temps_ecoule = now.hour * 3600 + now.minute * 60 + now.second    # Calculer le temps écoulé depuis minuit en secondes
+
+    cycle_total = time_on + time_off
+
+    if temps_ecoule % cycle_total < time_on:  #si temps ecoulé divisible par temp cycle on change de status
+        GPIO.output(pump1Pin, GPIO.HIGH)
+        return "pump1Pin: HIGH"
+    else:
+        GPIO.output(pump1Pin, GPIO.LOW)
+        return "pump1Pin: LOW"
 
 def read_tds():
     analog_value = GPIO.input(tdsPin)
-    voltage = analog_value / ADC_RESOLUTION * VREF
-    tds_value = voltage # Ici, insérez la formule correcte pour calculer la valeur TDS
-    return tds_value
+    #voltage = analog_value / ADC_RESOLUTION * VREF
+    #tds_value = voltage 
+    return analog_value
+
 
 try:
     while True:
         #pump control by user
-        pump_interval = read_pump_interval()
-
+        pump1_status()
 
         # Gestion TDS (exemple)
         tds = read_tds()
@@ -77,7 +72,4 @@ try:
         time.sleep(5)  # Pause de 5 secondes
 
 except KeyboardInterrupt:
-    pwm_led1.stop()
-    pwm_led2.stop()
-    pwm_led3.stop()
     GPIO.cleanup()
