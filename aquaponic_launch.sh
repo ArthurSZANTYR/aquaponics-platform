6295c04bb2de4c345d7b7d7002b279ad4daf099b
ArthurSZANTYR <szantyrarthur.pto@gmail.com>
2024-03-26 11:36:22 +0100

    auto website launch on shell

--- aquaponic_launch.sh
+++ aquaponic_launch.sh
@@ -6,6 +6,7 @@ cd aquaponics-platform
 source ./aqua-venv/bin/activate
 cd webserver
 python3 gpio.py &
-python3 webserver.py
+python3 webserver.py &
+xdg-open http://172.21.73.18:8000/index.html
 
 
