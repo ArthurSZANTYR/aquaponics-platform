9b67728289bc9c267c15f53bf287d540d77fd1e9
ArthurSZANTYR <szantyrarthur.pro@gmail.com>
2024-03-26 11:26:30 +0100

    version ok

--- aquaponic_launch.sh
+++ aquaponic_launch.sh
@@ -2,10 +2,6 @@ echo "/////////...........Bienvenue............/////////"
 echo "............... Lancement de la ferme............."
 echo ".................................................."
 echo "//////////////////////////////////////////////////"
-<<<<<<< HEAD
-ls
-=======
->>>>>>> 97bf130aed03f5e720233489d0f380f1923b0c69
 cd aquaponics-platform
 source ./aqua-venv/bin/activate
 cd webserver
